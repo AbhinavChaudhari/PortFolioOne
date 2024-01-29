c3c5d08c14d00618ee3cb168bd5af87887ff28a3
Abhinav Chaudhary <52206714+AbhinavChaudhari@users.noreply.github.com>
2024-01-29 20:09:25 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./app.scss"
+import Contact from "./components/contact/Contact"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
+import Portfolio from "./components/portfolio/Portfolio"
+import Services from "./components/services/Services"
 
 function App() {
 
@@ -12,13 +15,13 @@ function App() {
           <Navbar />
           <Hero />
         </section>
-        <section id="Services"><Parallax type="services"/></section>
-        <section>Services</section>
-        <section id="Portfolio"><Parallax type="portfolio"/></section>
-        <section>Portfolio1</section>
-        <section>Portfolio2</section>
-        <section>Portfolio3</section>
-        <section id="Contact">Contact</section>
+        <section id="Services"><Parallax type="services" /></section>
+        <section><Services /></section>
+        <section id="Portfolio"><Parallax type="portfolio" /></section>
+        <Portfolio />
+        <section id="Contact">
+          <Contact />
+        </section>
       </div>
     </>
   )
